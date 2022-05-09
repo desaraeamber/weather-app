0b2131f575588d1e9621087c661fbb75dd3d4151
desaraeamber <desaraealesley@gmail.com>
2022-05-08 21:37:12 -0400

    Made icons update with location weather

--- src/index.js
+++ src/index.js
@@ -60,9 +60,12 @@ function showTemp(response) {
   humidity.innerHTML = response.data.main.humidity;
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
-  //Icons
-  let icon = document.querySelector(".icon")
-  console.log(response);
+  //Show icon
+  let icon = document.querySelector(".icon");
+  icon.setAttribute(
+    "src", 
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  )
   }
 
 
